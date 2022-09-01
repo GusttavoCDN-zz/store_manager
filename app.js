c878da8fd6a5e77e480a5bb0663e2577fbe6a8a2
Gustavo Santos <gusttavo.x.santos@gmail.com>
2022-09-01 18:31:44 +0000

    docs: Iniciado criação da documentação do projeto StoreManager

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const cors = require('cors');
 const errorMiddleware = require('./middlewares/errorMiddleware');
 const productsRouter = require('./routes/productsRouter');
 const salesRouter = require('./routes/salesRouter');
+const swaggerFile = require('./swagger_output.json');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use('/products', productsRouter);
 app.use('/sales', salesRouter);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(errorMiddleware);
 
--- database/index.js
+++ database/index.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2/promise');
-require('dotenv').config(); 
+require('dotenv').config();
 
 const db = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'root',
   database: process.env.MYSQL_DATABASE || 'StoreManager',
+  port: process.env.MYSQL_PORT || '3308',
 });
 
 module.exports = db;
